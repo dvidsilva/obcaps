5e67b0a52c36a2a54b4752333bb7728055d6ca0f
Dvid Silva <dsilva@doctorondemand.com>
2014-11-01 20:30:37 -0700

    Created file structure

---
+++ js/obnoxious.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+  $('.obnoxise').on('click', function () {
+    obnoxise($('#text').val(),$('#obnoxious'));
+  });
+});
+
+
+// STRING src, jQueryElement dest
+var obnoxise = function (src, dest) {
+
+};
